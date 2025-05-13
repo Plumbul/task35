143ca9eaa1ba22deff27998ed52eaff6e53c1858
Plumbul <kirilglybokiy@gmail.com>
2025-05-13 17:15:11 +0300

    Fix incorrect output

--- task35.cc
+++ task35.cc
@@ -28,7 +28,7 @@ void algoritm_bfs(vector<vector<char>> &test_task0, int row_start, int col_start
             int col_next = col_current + col_offsets[move];
 
             if (row_next >= 0 && row_next < rows && col_next >= 0 && col_next < cols && !visited[row_next][col_next]){
-                if (test_task0[row_next][col_next] == '0' || (row_next == row_end && col_next == col_end)){
+                if (result[row_next][col_next] == '0' || (row_next == row_end && col_next == col_end)){
                     visited[row_next][col_next] = true;
                     q.push({row_next, col_next});
                     parent[row_next][col_next] = row_current * cols + col_current;
@@ -42,7 +42,7 @@ void algoritm_bfs(vector<vector<char>> &test_task0, int row_start, int col_start
         int col_current = col_end;
 
         while(row_current != row_start || col_current != col_start){
-            if(test_task0[row_current][col_current] == '0'){
+            if(result[row_current][col_current] == '0'){
                 int parent_cell_index = parent[row_current][col_current];
                 int row_parent = parent_cell_index / cols;
                 int col_parent = parent_cell_index % cols;
@@ -127,7 +127,7 @@ int main() {
 
         for(int i = 0; i < rows; i++){
             for(int j = 0; j < cols; j++){
-                if(test_task0[i][j] == symbol){
+                if(result[i][j] == symbol){
                     symbols_position.push_back({i, j});
                 }
             }
